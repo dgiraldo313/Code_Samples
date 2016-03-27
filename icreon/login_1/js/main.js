aa7cbc8658e27a4557a5867085efc2274ab4cba0
dgiraldo313 <dgiraldo313@drew.edu>
2016-03-27 16:29:21 -0400

    final commit
    still need to fix shadow

--- icreon/login_1/js/main.js
+++ icreon/login_1/js/main.js
@@ -2,6 +2,7 @@ var addButton= document.getElementsByClassName("add")[0];
 var container= document.getElementsByClassName("container")[0];
 var close= document.getElementsByClassName("close")[0];
 
+console.log(document.getElementsByClassName("container"));
 addButton.addEventListener("click", function(){
     container.className += " " + "active";
 });
